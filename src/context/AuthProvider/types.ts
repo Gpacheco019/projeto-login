02335d750264cdc728f13678a18c2cf09c7aa604
Gabriel Pacheco <gabrielpacheco.wddigital@gmail.com>
2022-02-01 07:30:19 -0300

    Initial Project-Login

---
+++ src/context/AuthProvider/types.ts
@@ -0,0 +1,14 @@
+export interface IUser {
+  email?: string;
+  token?: string;
+
+}
+
+export interface IContext extends IUser {
+  authenticate: (emial: string, password: string) => Promise<void>;
+  logout: () => void;
+}
+
+export interface IAuthProvider {
+  children: JSX.Element;
+}
